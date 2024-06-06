c0c2265c2bc114327428d799d11480f9a4f187e3
Leonardo Wiese <wiese.programmer@gmail.com>
2024-06-06 18:03:32 -0300

    entrega

--- cypress.config.js
+++ cypress.config.js
@@ -3,8 +3,8 @@ const { defineConfig } = require('cypress');
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-    
+      
     },
-    baseUrl: 'http://localhost:3000',
+    baseUrl: 'http://localhost:3000', 
   },
 });
